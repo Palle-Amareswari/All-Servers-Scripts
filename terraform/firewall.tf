a019ebe25d693549b2903e24fddd5e15946c58c2
Palle-Amareswari <92499127+Palle-Amareswari@users.noreply.github.com>
2026-04-06 13:34:22 +0530

    Update security group ID for ingress rule

--- terraform/firewall.tf
+++ terraform/firewall.tf
@@ -13,7 +13,7 @@ resource "aws_security_group" "allow_tls" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = "aws_security_group.my_sg.id"
+  security_group_id = "aws_security_group.allow_tls.id"
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   ip_protocol       = "tcp"
