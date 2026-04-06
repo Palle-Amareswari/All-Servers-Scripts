3e2b52d056fb6323dadb7d070b317564d9b35257
Palle-Amareswari <92499127+Palle-Amareswari@users.noreply.github.com>
2026-04-06 17:27:42 +0530

    Add Terraform configuration for Elastic IP and security group

---
+++ terraform/elastic_ip_sg.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_eip" "lb" {
+  domain = "vpc"
+}
+
+resource "aws_security_group" "allow_tls" {
+  name = "attribute-firewall"
+}
+
+resource "aws_vpc_security_group _ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4 = "{aws_eip.lb.public_ip}/32"
+  from_port = 443
+  ip_protocol = "tcp"
+  to_port = 443
+}
+
