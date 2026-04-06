7e71739ad4d0ac6c27f6718c6c27d7cd6203736d
Palle-Amareswari <92499127+Palle-Amareswari@users.noreply.github.com>
2026-04-06 17:43:18 +0530

    Fix resource name and update CIDR syntax

--- terraform/elastic_ip_sg.tf
+++ terraform/elastic_ip_sg.tf
@@ -10,9 +10,9 @@ resource "aws_security_group" "allow_tls" {
   name = "attribute-firewall"
 }
 
-resource "aws_vpc_security_group _ingress_rule" "allow_tls_ipv4" {
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4 = "{aws_eip.lb.public_ip}/32"
+  cidr_ipv4 = "${aws_eip.lb.public_ip}/32"
   from_port = 443
   ip_protocol = "tcp"
   to_port = 443
